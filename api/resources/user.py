9e3b5018b3ee7ec396cbb87d5f37b9a66ec986e6
Peter Kharikhin <Holysilver@gmail.com>
2022-02-10 20:40:32 +0300

    Swagger apispec

--- api/resources/user.py
+++ api/resources/user.py
@@ -1,9 +1,13 @@
 from api import Resource, abort, reqparse, auth
 from api.models.user import UserModel
-from api.schemas.user import user_schema, users_schema
+from api.schemas.user import user_schema, users_schema, UserSchema, UserRequestSchema
+from flask_apispec.views import MethodResource
+from flask_apispec import marshal_with, use_kwargs, doc
 
 
-class UserResource(Resource):
+@doc(description='Api for notes.', tags=['Users'])
+class UserResource(MethodResource):
+    @marshal_with(UserSchema, code=200)
     def get(self, user_id):
         # language=YAML
         """
@@ -40,11 +44,10 @@ class UserResource(Resource):
                             description: The role of the user
                             default: simple user
         """
-
         user = UserModel.query.get(user_id)
         if not user:
             abort(404, error=f"User with id={user_id} not found")
-        return user_schema.dump(user), 200
+        return user, 200
 
     @auth.login_required(role="admin")
     def put(self, user_id):
@@ -68,18 +71,17 @@ class UserResource(Resource):
         raise NotImplemented  # не реализовано!
 
 
-class UsersListResource(Resource):
+@doc(description='Api for notes.', tags=['Users'])
+class UsersListResource(MethodResource):
     def get(self):
         users = UserModel.query.all()
         return users_schema.dump(users), 200
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("username", required=True)
-        parser.add_argument("password", required=True)
-        user_data = parser.parse_args()
-        user = UserModel(**user_data)
+    @use_kwargs(UserRequestSchema, location=('json'))
+    @marshal_with(UserSchema, code=201)
+    def post(self, **kwargs):
+        user = UserModel(**kwargs)
         user.save()
         if not user.id:
             abort(400, error=f"User with username:{user.username} already exist")
-        return user_schema.dump(user), 201
+        return user, 201
--- api/schemas/user.py
+++ api/schemas/user.py
@@ -5,12 +5,21 @@ from api.models.user import UserModel
 #       schema        flask-restful
 # object ------>  dict ----------> json
 
-
+# json ----> dict
 # Сериализация ответа(response)
 class UserSchema(ma.SQLAlchemySchema):
     class Meta:
         model = UserModel
         fields = ('id', 'username', 'is_staff', 'role')
+
+
+# Десериализация запроса(request)
+class UserRequestSchema(ma.SQLAlchemySchema):
+    class Meta:
+        model = UserModel
+
+    username = ma.Str()
+    password = ma.Str()
 
 
 user_schema = UserSchema()
